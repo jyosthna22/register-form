34c045383e8ef626c34d32c2c3e8664bbd430689
Venkata Naga Jostna Padarthi <jyosthnapadarthi22@gmail.com>
2025-10-28 21:11:36 +0530

    Update index2.py

--- index2.py
+++ index2.py
@@ -8,7 +8,7 @@ st.form("registration_form")
 name = st.text_input("Full Name")
 email = st.text_input("Email")
 password = st.text_input("Password", type="password")
-gender = st.radio("Gender", ["Male", "Female", "Other"])
+gender = st.radio("Gender", ["Male", "Female", "Other"],index=None)
 course = st.multiselect(
     "Select your course:",
     ["Python", "SQL", "Web Development", "Data Science","C","Java","c++","Data Analyst"]
@@ -19,4 +19,5 @@ submit = st.button("Register")
 
 if submit:
     st.success(f"Registration successful! Welcome, {name}.")
+
 
