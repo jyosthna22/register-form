d7d475827987e272de9a01b6c456c4b6751513d6
Venkata Naga Jostna Padarthi <jyosthnapadarthi22@gmail.com>
2025-10-28 21:03:59 +0530

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+import streamlit as st
+
+st.title(" Registration Form")
+
+st.form("registration_form")
+name = st.text_input("Full Name")
+email = st.text_input("Email")
+password = st.text_input("Password", type="password")
+gender = st.radio("Gender", ["Male", "Female", "Other"])
+course = st.multiselect(
+    "Select your course:",
+    ["Python", "SQL", "Web Development", "Data Science","C","Java","c++","Data Analyst"]
+)
+agree = st.checkbox("I agree to the Terms & Conditions")
+
+submit = st.button("Register")
+
+if submit:
+    st.success(f"Registration successful! Welcome, {name}.")
+
